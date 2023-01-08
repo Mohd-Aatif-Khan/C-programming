9c1bd5274c8f3b832a3b5cb180aa803cad288015
Mohd Aatif Khan <114300008+Mohd-Aatif-Khan@users.noreply.github.com>
2023-01-08 16:32:19 +0530

    Add files via upload

---
+++ 9calloc.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int n;
+    printf("enter size of array \n");
+    scanf("%d",&n);
+
+    int *a = (int*)calloc(n,sizeof (int));
+
+    for(int i=0;i<n;i++){
+        a[i]=i+1;
+    }
+    for(int i=0;i<n;i++){
+        printf("%d",a[i]);
+    }
+}
